04864be15e84cafa7627f23924db1a7023c7d3b0
Simone <83299333+simonexxiv@users.noreply.github.com>
2026-04-01 16:23:01 +0200

    Delete Produttore_Consumatore/Singolo_Buffer directory

--- Produttore_Consumatore/Singolo_Buffer/procedure.h
+++
@@ -1,9 +0,0 @@
-#define SPAZIO_DISPONIBILE 0
-#define MESSAGGIO_DISPONIBILE 1
-
-struct condivisa{
-    int valore;
-};
-
-void produttore(struct condivisa *s, int sem_id);
-void consumatore(struct condivisa *s, int sem_id);
